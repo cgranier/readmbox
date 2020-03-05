151ca21f9024377f5d260a3940041fd7c4496dbd
Carlos Granier <granier@gmail.com>
2020-03-05 16:21:57 -0500

    Write data out to CSV file

--- readmbox_claim.py
+++ readmbox_claim.py
@@ -1,5 +1,7 @@
 import mailbox
 import re
+import csv
+
 
 mbox_file = "data/CLAIM.mbox"
 mbox = mailbox.mbox(mbox_file)
@@ -20,39 +22,47 @@ def getbody(message): #getting plain text 'email body'
 
 def main():
     total_messages = 1
-    for message in mbox:
-        print("Message #:", total_messages)
-        # print("from   :",message['from'])
-        print("date   :",message['date'])
-        # print("subject:",message['subject'])
-        print("Channel Name:",re.search('(?<=Hi\s)(?:(.+?),)',getbody(message).decode("utf-8")).group(1))
-        # print("Video title:",re.search('(?<=Video title:\s)(?:(.+?)\\r\\n)',getbody(message).decode("utf-8")).group(1))
-        if (video_title := re.search('(?<=Video title:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
-            print("Video title:", video_title.group(1))
-        else:
-            print("message: ",getbody(message).decode("utf-8"))
+    claim_list_file = "data/claim_list.csv"
+    with open(claim_list_file, 'w', newline='', encoding='utf-8') as new_file:
+        out_file_headers = ['channel_name','video_title','copyrighted_content','claimed_by','claim_note','claim_url','claim_date']
+        out_writer = csv.DictWriter(new_file, fieldnames = out_file_headers)
+        out_writer.writeheader()
+        for message in mbox:
+            claim_date = message['date']
+            # print("Channel Name:",re.search('(?<=Hi\s)(?:(.+?),)',getbody(message).decode("utf-8")).group(1))
+            if (channel_name := re.search('(?<=Hi\s)(?:(.+?),)',getbody(message).decode("utf-8"))) is not None:
+                channel_name = channel_name.group(1)
+            else:
+                channel_name = "N/A"
+            # print("Video title:",re.search('(?<=Video title:\s)(?:(.+?)\\r\\n)',getbody(message).decode("utf-8")).group(1))
+            if (video_title := re.search('(?<=Video title:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
+                video_title = video_title.group(1)
+            else:
+                video_title = "N/A"
+            # print("Copyrighted content:",re.search('(?<=Copyrighted content:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
+            if (copyrighted_content := re.search('(?<=Copyrighted content:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
+                copyrighted_content = copyrighted_content.group(1)
+            else:
+                copyrighted_content = "N/A"
+            # print("Claimed by:",re.search('(?<=Claimed by:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
+            if (claimed_by := re.search('(?<=Claimed by:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
+                claimed_by = claimed_by.group(1)
+            else:
+                claimed_by = "N/A"
+            # print("Claim details:",re.search('(?<=View claim details:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
+            if (claim_details := re.search('(?<=View claim details:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
+                claim_details = claim_details.group(1)
+            else:
+                claim_details = "N/A"
+            # print("Note:",re.search('(?<=Note:\s)(?:(.+?)\\r\\n\\r)',getbody(message).decode("utf-8")).group(1))
+            if (claim_note := re.search('(?<=Note:\s)(?:(.+?)\r)',getbody(message).decode("utf-8"))) is not None:
+                claim_note = claim_note.group(1)
+            else:
+                claim_note = "N/A"
             
-        # print("Copyrighted content:",re.search('(?<=Copyrighted content:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
-        if (copyrighted_content := re.search('(?<=Copyrighted content:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
-            print("Copyrighted content:", copyrighted_content.group(1))
+            out_writer.writerow({'channel_name' : channel_name,'video_title' : video_title,'copyrighted_content' : copyrighted_content,'claimed_by' : claimed_by,'claim_note' : claim_note,'claim_url' : claim_details,'claim_date' : claim_date})
 
-        # print("Claimed by:",re.search('(?<=Claimed by:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
-        if (claimed_by := re.search('(?<=Claimed by:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
-            print("Claimed by:", claimed_by.group(1))
-            
-        # print("Claim details:",re.search('(?<=View claim details:\s)(?:(.+?)\n)',getbody(message).decode("utf-8")).group(1))
-        if (claim_details := re.search('(?<=View claim details:\s)(?:(.+?)\n)',getbody(message).decode("utf-8"))) is not None:
-            print("Claim details:", claim_details.group(1))
-            
-        # print("Note:",re.search('(?<=Note:\s)(?:(.+?)\\r\\n\\r)',getbody(message).decode("utf-8")).group(1))
-        if (claim_note := re.search('(?<=Note:\s)(?:(.+?)\\r\\n\\r)',getbody(message).decode("utf-8"))) is not None:
-            print("Note:", claim_note.group(1))
-        
-        print("**************************************")
-
-        total_messages +=1
-
-
+            total_messages +=1
 
 if __name__ == "__main__":
     main()
